336ba7e10b242e017a7910f830b52eae998d5ef4
Ryan Kracaw <125154654+ryankracaw@users.noreply.github.com>
2023-04-25 12:46:08 -0500

    Add files via upload

---
+++ VacationPy/geo_key.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "834e67a2eb83f6a885c0eb808cc81d6b"
+
+# Geoapify API Key
+geoapify_key = "f4ef75790deb43d0898b982ee9d43643"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "834e67a2eb83f6a885c0eb808cc81d6b"
+
+# Geoapify API Key
+geoapify_key = "YOUR KEY HERE"
